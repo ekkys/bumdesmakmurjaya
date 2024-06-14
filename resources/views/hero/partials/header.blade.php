c526cc3d5b3ffd58ec89bdefb8388c51baaaed89
Ekky Sukmawati <ekkys99@gmail.com>
2024-06-14 19:59:17 +0700

    Restrukturisasi Folder

---
+++ resources/views/hero/partials/header.blade.php
@@ -0,0 +1,30 @@
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+
+        <a href="" class="logo d-flex align-items-center me-auto">
+            <img src="assets/img/bumdes.png" alt="">
+            <h1 class="sitename">Bumdes Makmur Jaya</h1>
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+            <ul>
+                <li><a href="#hero" class="">Home</a></li>
+                <li class="dropdown"><a href="#about"><span>Tentang Kami</span> <i
+                            class="bi bi-chevron-down toggle-dropdown"></i></a>
+                    <ul>
+                        <li><a href="#featured-services">Legalitas</a></li>
+                    </ul>
+                </li>
+                <li><a href="#features">Unit Kami</a></li>
+                <li><a href="#features-details">Layanan</a></li>
+                <li><a href="#clients">Klien</a></li>
+                <li><a href="#pricing">Pricing</a></li>
+                <li><a href="#contact">Kontak</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+        <a class="btn-getstarted" href="#about">Get Started</a>
+
+    </div>
+</header>
