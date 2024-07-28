22e35989705475f8379c6bf552440d17faa6414d
Ekky Sukmawati <ekkys99@gmail.com>
2024-07-28 17:06:30 +0700

    remove breeze

--- routes/web.php
+++ routes/web.php
@@ -44,15 +44,3 @@ Route::resource('layanan', LayananController::class);
 Route::resource('biaya', BiayaController::class);
 Route::resource('kontak', KontakController::class);
 Route::resource('galeri', GaleriController::class);
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// require __DIR__ . '/auth.php';
