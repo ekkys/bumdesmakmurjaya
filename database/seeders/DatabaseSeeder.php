99a8a17a59dda0256dcc1de0d7228f70081fc37f
Ekky Sukmawati <ekkys99@gmail.com>
2026-08-09 18:08:18 +0700

    feat: initialize DatabaseSeeder with site module seeders and test user creation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             KontakSeeder::class,
             BiayaSeeder::class,
             UserSeeder::class,
+            BeritaSeeder::class,
         ]);
 
 
